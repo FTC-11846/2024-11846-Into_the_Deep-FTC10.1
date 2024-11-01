23e3bbb54a74bccc1f9dcf1a68597c071bd3a7e0
LouisHarnish <louisharnish0@gmail.com>
2024-11-01 16:33:24 -0400

    Fixed Gradle and got robot functioning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmodes/Teleop.java
@@ -27,28 +27,45 @@ public class Teleop extends OpMode {
     public void loop(){
 
         double forward = -gamepad1.left_stick_y;
-        double turn = -gamepad1.right_stick_x;
-        double strafe = gamepad1.left_stick_x;
+        double turn = gamepad1.right_stick_x;
+        double strafe = -gamepad1.left_stick_x;
         double lift = gamepad2.left_stick_y;
         double extend = -gamepad2.right_stick_y;
 
         if(gamepad2.a){
-            intakeSubsystem.intake(1);
-        } else if(gamepad2.b){
             intakeSubsystem.intake(-1);
+        } else if(gamepad2.b){
+            intakeSubsystem.intake(1);
         } else {
             intakeSubsystem.intake(0);
         }
 
         if(gamepad2.x){
-            intakeSubsystem.sampleDropper(1);
-        }else{
-            intakeSubsystem.sampleDropper(0);
+            intakeSubsystem.sampleDropper(0.05);
+        }else if(gamepad2.y){
+            intakeSubsystem.sampleDropper(0.89);
         }
+
+//        if(12 < scissorSubsystem.scissorLCounts()< 15){
+//            intakeSubsystem.sampleDropper(#);
+//        }else if( 15 < scissorSubsystem.scissorLCounts() < 25){
+//            intakeSubsystem.sampleDropper(#);
+//        }
+
+        if(gamepad2.dpad_down){
+            scissorSubsystem.hang();
+        }else {
+            scissorSubsystem.openHang();
+        }
+
 
         mecanumSubsystem.TeleOperatedDrive(forward, -strafe, turn);
         scissorSubsystem.scissorLift(lift);
         intakeSubsystem.extend(extend);
+
+        telemetry.addData("ScissorLCounts", scissorSubsystem.scissorLCounts());
+        telemetry.addData("ScissorRCounts", scissorSubsystem.scissorRCounts());
+        telemetry.update();
     }
 
 }
